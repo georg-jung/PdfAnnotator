df3bfe626c291c79b3e4d7c259f79a6d7ec5a830
Georg Jung <git@gjung.com>
2019-01-27 19:20:10 +0100

    add some dictionary helper extensions

---
+++ PdfAnnotator/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfAnnotator
+{
+    internal static class Extensions
+    {
+        public static TValue GetOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key,
+            TValue defaultValue = default)
+        {
+            if (dict.TryGetValue(key, out var val)) return val;
+            return defaultValue;
+        }
+
+        public static TValue AddAndReturn<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue value)
+        {
+            dict.Add(key, value);
+            return value;
+        }
+    }
+}
