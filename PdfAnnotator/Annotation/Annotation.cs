35dffd400d15cb3a9957a9eab3214eb71f23916d
Georg Jung <git@gjung.com>
2019-01-27 19:52:26 +0100

    add wip-annotation datastructures

---
+++ PdfAnnotator/Annotation/Annotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using PdfAnnotator.Words;
+
+namespace PdfAnnotator.Annotation
+{
+    internal class Annotation : IAnnotation
+    {
+        public Annotation(IWord subject)
+        {
+            Subject = subject;
+        }
+
+        public IWord Subject { get; }
+        public string Content { get; set; }
+        public List<Pdf.IWord> SelectedTargets { get; set; }
+
+        IReadOnlyList<Pdf.IWord> IAnnotation.SelectedTargets => SelectedTargets;
+    }
+}
---
+++ PdfAnnotator/Annotation/IAnnotation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PdfAnnotator.Words;
+
+namespace PdfAnnotator.Annotation
+{
+    internal interface IAnnotation
+    {
+        IWord Subject { get; }
+        string Content { get; }
+        IReadOnlyList<Pdf.IWord> SelectedTargets { get; }
+    }
+}
